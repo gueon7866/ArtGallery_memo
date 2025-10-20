25dde7fb3ff56de26632138c06aa2823bc0173bb
gueon7866 <lsg7866@gmail.com>
2025-10-20 21:23:08 +0900

    1
    11

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const cookieParser = require('cookie-parser');
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT
+app.use(cors());
+app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
 
-app.use(cors({
-  origin: process.env.FRONT_ORIGIN,
-  credentials: true
-}));
-app.use(express.json({ limit: "2mb" }));
-app.use(cookieParser());
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB 연결 성공"))
-  .catch((err) => console.error("MongoDB 연결 실패:", err.message));
-
-
-app.get("/", (_req, res) => res.send("PhotoMemo API OK"));
-
-
-app.use((req, res) => {
-
-  res.status(500).json({ message: "서버 오류"});
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running: http://localhost:${PORT}`);
-
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
